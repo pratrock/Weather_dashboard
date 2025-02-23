61ae29653588e8198422ecda158a75ef69f4b39c
Prateek Parmar <30598262+pratrock@users.noreply.github.com>
2025-02-23 16:27:45 -0600

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import Layout from "./components/Layout";
 import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/WeatherDashboard";
 import CityPage from "./pages/CityPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard />}></Route>
-            <Route path="/city/:cityName" element={<CityPage />}></Route>
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />}></Route>
+              <Route path="/city/:cityName" element={<CityPage />}></Route>
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { RefreshCw } from "lucide-react";
 import React from "react";
 
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard</div>;
+  return (
+    <div>
+      <h1>My Location</h1>
+      <Button variant={"outline"} size={"icon"}>
+        <RefreshCw />
+      </Button>
+    </div>
+  );
 };
 
 export default WeatherDashboard;
